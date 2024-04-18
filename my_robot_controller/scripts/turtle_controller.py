250e415fc1bb053558d1184d31ddb49b652a60db
Vivek Sai <113660738+VivekSai07@users.noreply.github.com>
2024-04-18 15:15:30 +0530

    Add files via upload

--- my_robot_controller/scripts/turtle_controller.py
+++ my_robot_controller/scripts/turtle_controller.py
@@ -3,6 +3,18 @@
 import rospy
 from turtlesim.msg import Pose
 from geometry_msgs.msg import Twist
+from turtlesim.srv import SetPen
+
+previous_x = 0
+previous_y = 0
+
+def call_set_pen_service(r, g, b, width, off):
+    try:
+        set_pen = rospy.ServiceProxy("/turtle1/set_pen", SetPen)
+        response = set_pen(r, g, b, width, off)
+        # rospy.loginfo(response)
+    except rospy.ServiceException as e:
+        rospy.logwarn(e)
 
 def pose_callback(pose: Pose):
     cmd = Twist()
@@ -14,10 +26,18 @@ def pose_callback(pose: Pose):
         cmd.angular.z = 0.0
     pub.publish(cmd)
 
+    global previous_x
+    if (pose.x >= 5.5 and previous_x < 5.5): # Right
+        rospy.loginfo(f"Set color to red! ({pose.x}, {pose.y})")
+        call_set_pen_service(255, 0, 0, 3, 0)
+    elif (pose.x < 5.5 and previous_x > 5.5): # Left
+        rospy.loginfo(f"Set color to blue! ({pose.x}, {pose.y})")
+        call_set_pen_service(0, 0, 255, 3, 0)
+    previous_x = pose.x
 
 if __name__ == '__main__':
     rospy.init_node("turtle_controller")
-
+    rospy.wait_for_service("/turtle1/set_pen")
     pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
     sub = rospy.Subscriber("/turtle1/pose", Pose, callback=pose_callback)
     rospy.loginfo("Node has been started")
